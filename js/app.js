86a023a20b4917874ab357e4fb9945827f5ca78f
nsolimine <32491766+nsolimine@users.noreply.github.com>
2018-01-23 15:04:15 -0700

    url change

--- js/app.js
+++
@@ -1,7 +0,0 @@
-var randomColor = Math.floor(Math.random()*16777215).toString(16);
-
-
-var hexArray = ['#hexVal','#hexVal','#hexval', '#hexval']
-var randomColor = hexArray[Math.floor(Math.random() * hexArray.length)];
-
-$("#color").css("color",randomColor);
